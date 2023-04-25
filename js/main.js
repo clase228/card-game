5b1c848e7f9e9c250276c38556453e85e28d58c6
glooorp <nik.roppel@mail.ru>
2023-04-25 20:52:42 +0600

    lesson 3

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
-let wrapper = document.querySelector('.wrapper') 
-let lvlBtns = document.querySelectorAll('.difficulty_level_num') 
-let startBtn = document.querySelector('.difficulty_start') 
-let difficulty;
-
+let wrapper = document.querySelector('.wrapper')
+let lvlBtns = document.querySelectorAll('.difficulty_level_num')
+let startBtn = document.querySelector('.difficulty_start')
+let difficulty
 
 startBtn.addEventListener('click', function () {
-   let activeLvl =document.querySelector('.difficulty_level_num.active')
+   let activeLvl = document.querySelector('.difficulty_level_num.active')
    if (activeLvl) {
       difficulty = activeLvl.dataset.lvl
       request({
-         url:'test.html',
-         responseType:'',
-         onSuccess:(data)=>{
+         url: 'test.html',
+         responseType: '',
+         onSuccess: (data) => {
             wrapper.innerHTML = data
-            document.querySelector('.difficultyLvl').innerHTML = difficulty
-
+            let html = ''
+            for (let i = 0; i < 36; i++) {
+               html += `
+               <div class="card-icon" data-count="${i}">
+                  <img src="img/back-card.png" alt="">
+               </div>
+               `
+            }
+            document.querySelector('.card-icons').innerHTML = html
          },
-         onError:(data)=>{
-            console.log(data);
-         }
-
+         onError: (data) => {
+            console.log(data)
+         },
       })
-   }else{
-      console.log('Ошибка!');
+   } else {
+      console.log('Ошибка!')
    }
-   
-   
-}) 
+})
 for (let i = 0; i < lvlBtns.length; i++) {
    function hide() {
       for (let el = 0; el < lvlBtns.length; el++) {
@@ -37,5 +40,4 @@ for (let i = 0; i < lvlBtns.length; i++) {
       hide()
       lvlBtns[i].classList.add('active')
    })
-   
 }
